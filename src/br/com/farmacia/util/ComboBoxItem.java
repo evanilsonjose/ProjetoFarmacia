bb7ab04b62aa32364511028b03459ee6a5253559
evanilson <evanilson@192.168.0.102>
2015-05-19 03:14:25 -0300

    atualização do pacote teste e util

---
+++ src/br/com/farmacia/util/ComboBoxItem.java
@@ -0,0 +1,37 @@
+package br.com.farmacia.util;
+
+public class ComboBoxItem {
+
+    private int codigo;
+    private String nome;
+
+    public ComboBoxItem() {
+    }
+
+    public ComboBoxItem(int codigo, String nome) {
+        this.codigo = codigo;
+        this.nome = nome;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return nome;
+    }
+    
+}
