aa68a657ef64dffb6852fcf422897712c9ae3403
Rodrigo e Carol <Rodrigo e Carol@RodrigoCarol>
2015-05-22 12:33:34 -0300

    telas alterar e consulta funcionario editadas

--- src/br/com/farmacia/gui/TelaConsultaFuncionario.java
+++ src/br/com/farmacia/gui/TelaConsultaFuncionario.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.com.farmacia.gui;
+
 
 import br.com.farmacia.beans.Funcionario;
 import br.com.farmacia.dao.FuncionarioDAO;
@@ -308,6 +308,7 @@ public class TelaConsultaFuncionario extends javax.swing.JFrame {
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(TelaConsultaFuncionario.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
